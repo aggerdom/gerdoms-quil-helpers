49e4ebaadd0a1080e31b7b781a84f647131e6410
Alex Gerdom <alexgerdom@gmail.com>
2021-01-17 22:07:23 -0500

    feat(scaffolding): Layout library

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to gerdoms-quil-helpers
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/gerdoms_quil_helpers/core_test.clj
@@ -0,0 +1,7 @@
+(ns gerdoms-quil-helpers.core-test
+  (:require [clojure.test :refer :all]
+            [gerdoms-quil-helpers.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
