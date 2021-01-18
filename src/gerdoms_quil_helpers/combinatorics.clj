0bc28b5d9c304055379d40870ff70b4fa72f7329
Alex Gerdom <alexgerdom@gmail.com>
2021-01-17 23:56:56 -0500

    feat(imports): Fix imports

--- src/gerdoms_quil_helpers/combinatorics.clj
+++ src/gerdoms_quil_helpers/combinatorics.clj
@@ -8,7 +8,7 @@
              rest (k-combination (dec k) (clojure.set/difference S #{el}))]
          (conj rest el)))))
 
-(deftest -k-combination-test
-  (testing "Examples from site"
-    (is (= (k-combination 2 #{4 5 6})
-           #{#{6 5} #{4 6} #{4 5}}))))
+;; (deftest -k-combination-test
+;;   (testing "Examples from site"
+;;     (is (= (k-combination 2 #{4 5 6})
+;;            #{#{6 5} #{4 6} #{4 5}}))))
--- src/gerdoms_quil_helpers/core.clj
+++ src/gerdoms_quil_helpers/core.clj
@@ -1,3 +1,13 @@
 (ns gerdoms-quil-helpers.core
-  (:require [quil.core :as q]
+  (:require [gerdoms-quil-helpers.general :as general]
+            [gerdoms-quil-helpers.geometry :as geom]
+            [gerdoms-quil-helpers.combinatorics :as combo]
+            [gerdoms-quil-helpers.interpolation :as interp]
+            [gerdoms-quil-helpers.linear-algebra :as lin]
+            [gerdoms-quil-helpers.primes :as primes]
+            [gerdoms-quil-helpers.sequences :as sequences]
+            [gerdoms-quil-helpers.sets-relations :as setrel]
+            [gerdoms-quil-helpers.repl :as repl]
+            [quil.core :as q]
             [quil.middleware :as m]))
+
